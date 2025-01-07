ebd11d27deaaca2853751e4d0359dc23c2bd7c5d
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2025-01-07 02:09:57 -0800

    Update layout.js
    adding heading tags for logo

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <head>
+       <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
